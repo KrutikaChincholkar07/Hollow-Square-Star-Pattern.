1b600852edf7cf184952268da49a3bbdf048a770
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-09-28 16:51:56 +0530

    Create HollowSquare.java
    Hollow Rectangle (or Hollow Square) Star Pattern code in Java
    Output:
    * * * *
    *     *
    *     *
    * * * *

---
+++ HollowSquare.java
@@ -0,0 +1,15 @@
+public class HollowSquare {
+    public static void main(String[] args) {
+        int rows = 4, cols = 4;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
